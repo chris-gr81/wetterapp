462c3577c1427ce875f5def0971eaf746eb73463
Christian Grimm <grimmchristian@gmx.de>
2025-04-14 19:51:07 +0200

    Rename files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./styles.scss";
-import { renderAppCurrent } from "./ui_cityView";
+import { renderAppCurrent } from "./ui_city_view";
 
 export const appEl = document.querySelector(".app");
 
