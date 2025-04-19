855095b670c19a9a99350dccc2bfb091661dcbc0
Christian Grimm <grimmchristian@gmx.de>
2025-04-19 10:53:09 +0200

    Remove mkcert

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
-/* import mkcert from "vite-plugin-mkcert";*/
 
 export default defineConfig({
   base: "/wetterapp/",
-  /* plugins: [mkcert()],
-  server: {
-    https: true,
-  }, */
 });
